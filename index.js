4bc34580808f84baa9ace3d53d65b45d84719a2f
Bruno Silva <brunosilvinosilva@gmail.com>
2020-03-22 12:52:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+const map = (sourceArray, callBack) => {
+    let newArray = [] 
+    for (let i = 0; i < sourceArray.length; i++) {
+        let theElement = sourceArray[i]
+        newArray.push(callBack(theElement))
+      }
+    
+      return newArray;
+    }
+
+    const reduce = (sourceArray, callBack, startingPoint) => {
+
+        let newArray = (!!startingPoint) ? startingPoint : sourceArray[0]
+        let i = (!!startingPoint) ? 0 : 1
+      
+        for (; i < sourceArray.length; i++) {
+          newArray = callBack(sourceArray[i], newArray)
+        }
+      
+        return newArray;
+    }
